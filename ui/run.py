21a0d9a0031c06943b4e8ec19696d702a3aa8c20
vince <vincent.trolard@gmail.com>
2021-02-28 21:21:03 +0100

    Filter TreeWidget

--- ui/run.py
+++ ui/run.py
@@ -1,6 +1,6 @@
 import subprocess
 
-cmd = "C:\\Users\\vince\\AppData\\Roaming\\Python\\Python39\\Scripts\\pyside2-uic.exe graphic_view.ui > graphic_view.py"
+# cmd = "C:\\Users\\vince\\AppData\\Roaming\\Python\\Python39\\Scripts\\pyside2-uic.exe graphic_view.ui > graphic_view.py"
 cmd = "C:\\Users\\vince\\AppData\\Roaming\\Python\\Python39\\Scripts\\pyside2-uic.exe search.ui > search.py"
 
 subprocess.run(cmd, shell=True)
